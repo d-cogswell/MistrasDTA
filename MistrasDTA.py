d1caf0d47ab47ff94ef049569d1232892f59ef02
Dan Cogswell <cogswell@mit.edu>
2021-10-27 21:13:10 -0500

    Removes functions for loading exported text files.

--- MistrasDTA.py
+++ MistrasDTA.py
@@ -6,42 +6,6 @@ import logging
 logging.getLogger().setLevel(logging.ERROR)
 
 
-def read(file):
-
-    # Get data
-    AE_events = np.array(np.loadtxt(file, skiprows=8))
-
-    # Get fields from 8th line of file
-    f = open(file, 'r')
-
-    for line_num in range(1, 9):
-        line = f.readline()
-        if line_num == 8:
-            fields_str = line
-
-    f.close()
-
-    # Add field names for the columns
-    rec = np.core.records.fromrecords(AE_events, names=fields_str.split())
-
-    return(rec)
-
-
-def start_time(file):
-
-    # Get date from 6th line of file
-    f = open(file, 'r')
-
-    for line_num in range(1, 7):
-        line = f.readline()
-        if line_num == 6:
-            date = line
-
-    f.close()
-
-    return(datetime.strptime(date, '%a %b %d %H:%M:%S %Y\n'))
-
-
 CHID_to_str = {
     1: 'RISE',
     3: 'COUN',
