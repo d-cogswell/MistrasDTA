ef697468f902c392553aaa9a4684302c93aabc6b
Dan Cogswell <cogswell@mit.edu>
2021-10-29 12:41:40 -0400

    Adds long description to setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name="MistrasDTA",
     version="0.1.0",
     description="Read AEWin acoustic emissions binary data files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Dan Cogswell",
     author_email="cogswell@mit.edu",
     license="MIT",
