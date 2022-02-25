c358c57782a6adc51c070cb70891a3008d5ce488
Dan Cogswell <cogswell@mit.edu>
2022-02-25 18:43:21 -0500

    Include additional AE characteristics.

--- MistrasDTA.py
+++ MistrasDTA.py
@@ -8,20 +8,36 @@ logging.getLogger().setLevel(logging.ERROR)
 
 CHID_to_str = {
     1: 'RISE',
+    2: 'PCNTS',
     3: 'COUN',
     4: 'ENER',
     5: 'DURATION',
     6: 'AMP',
+    8: 'ASL',
+    10: 'THR',
+    13: 'A-FRQ',
+    17: 'RMS',
+    18: 'R-FRQ',
+    19: 'I-FRQ',
+    20: 'SIG STRENGTH',
     21: 'ABS-ENERGY',
     23: 'FRQ-C',
     24: 'P-FRQ'}
 
 CHID_byte_len = {
     1: 2,
+    2: 2,
     3: 2,
     4: 2,
     5: 4,
     6: 1,
+    8: 1,
+    10: 1,
+    13: 2,
+    17: 2,
+    18: 2,
+    19: 2,
+    20: 4,
     21: 4,
     23: 2,
     24: 2}
