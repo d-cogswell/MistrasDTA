5b9cbd817d50a3c23838d281be12f54f089fcaaa
Dan Cogswell <cogswell@alum.mit.edu>
2022-10-11 20:47:19 -0400

    Add a license file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name="MistrasDTA",
-    version="0.1.1",
+    version="0.1.2",
     description="Read AEWin acoustic emissions binary data files",
     long_description=long_description,
     long_description_content_type="text/markdown",
