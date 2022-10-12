4a6cb3354d806a3fb57007003bfb56dca0b1a745
Dan Cogswell <cogswell@alum.mit.edu>
2022-10-11 20:48:36 -0400

    Version 0.1.2.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 setup(
     name="MistrasDTA",
     version="0.1.2",
-    description="Read AEWin acoustic emissions binary data files",
+    description="Read AEWin acoustic emissions DTA files",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Dan Cogswell",
-    author_email="cogswell@mit.edu",
+    author_email="cogswell@alum.mit.edu",
     license="MIT",
     url="https://github.com/d-cogswell/MistrasDTA",
     py_modules=["MistrasDTA"],
